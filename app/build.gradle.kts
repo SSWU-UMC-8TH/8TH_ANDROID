3927e158a94ea1a8a07b07959e006e29baf1d934
Erna23 <trueyn03@gmail.com>
2025-04-05 15:06:04 +0900

    Add Week4 Android project

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -53,4 +53,5 @@ dependencies {
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
     implementation("me.relex:circleindicator:2.1.6")
+
 }
