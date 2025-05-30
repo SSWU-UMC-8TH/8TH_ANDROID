f43cb7dd45ab507c02118974ab2e665f3874f06b
gmin <gllom7884@gmail.com>
2025-05-30 15:24:37 +0900

    로그인 레이아웃 수정

--- app/src/main/java/com/example/flo/LoginActivity.kt
+++ app/src/main/java/com/example/flo/LoginActivity.kt
@@ -16,6 +16,7 @@ class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
+
         binding = ActivityLoginBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
